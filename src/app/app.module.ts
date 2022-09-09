e90dda43cc833ebdbd559433983b519d7bda8f9d
WalterJuarez <wjuarezm@miumg.edu.gt>
2022-09-09 12:46:29 -0600

    segundo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuarioDieselComponent } from './usuario-diesel/usuario-diesel.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuarioDieselComponent,
+    LoginComponent,
 
   ],
   imports: [
